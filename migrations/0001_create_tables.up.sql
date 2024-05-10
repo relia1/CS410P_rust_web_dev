6cb0fe469293e2bdd264f3fbb8ab14f5253f2e42
Robert Elia <relia@pdx.edu>
2024-05-09 22:23:55 -0700

    cascading delete

--- migrations/0001_create_tables.up.sql
+++ migrations/0001_create_tables.up.sql
@@ -16,7 +16,7 @@ CREATE TABLE question_tags (
 	question_id integer NOT NULL,
 	tag_id integer NOT NULL,
 	PRIMARY KEY (question_id, tag_id),
-	FOREIGN KEY (question_id) REFERENCES questions(id),
-	FOREIGN KEY (tag_id) REFERENCES tags(id)
+	FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE,
+	FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
